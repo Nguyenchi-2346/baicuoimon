60d89b65eb8a08149b9a33f91adbf0e0d51d094b
Nguyenchi-2346 <linhchi35343@gmail.com>
2025-07-01 22:38:50 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,15 @@
+// JavaScript Documentvar typed = new Typed('.element', {
+var typed = new Typed('.element', {
+strings: ["Web Designer.", "Code Developer.","Apps Designer.","Film Maker."],
+typeSpeed:50,
+backSpeed:50,
+loop:true,
+				});
+
+$('.timeline-panel').click(function() {
+    $('.timeline-body', this).toggle(); // p00f
+});
+
+$('.counter').countUp();
+
+AOS.init();
